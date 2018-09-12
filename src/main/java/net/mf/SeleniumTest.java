5a4b54a412d89a5bb79fd22726d209bc278dae54
panama69 <david.flynn@microfocus.com>
2018-09-12 16:33:13 +0000

    testing webdriver management proposed by Jason Hrabi

--- src/main/java/net/mf/SeleniumTest.java
+++ src/main/java/net/mf/SeleniumTest.java
@@ -1,6 +1,8 @@
 package net.mf;
 
 import java.io.File;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -31,8 +33,8 @@ public class SeleniumTest  {
     //You will need to have an account created in AOS and will need to supply the credentials
     private static final String ADV_LOGIN    = "Mercury"; //"insert login name here";
     private static final String ADV_PASSWORD = "Mercury"; //"insert password here";
-    private static final String DRIVER_VER = "2.41";
-    private static final String DRIVER_PATH="/"+DRIVER_VER+"/chromedriver";
+//    private static final String DRIVER_VER = "2.41";
+//    private static final String DRIVER_PATH="/"+DRIVER_VER+"/chromedriver";
 
     private static WebDriver driver;
     public SeleniumTest() {
@@ -41,16 +43,17 @@ public class SeleniumTest  {
 
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
-        File f = new File(System.getProperty("user.dir")+DRIVER_PATH);
-        if(f.exists() && !f.isDirectory()) {
-            // If you don't want to specify the path to the chrome driver, you will need to make sure it is part of the system path
-            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+DRIVER_PATH);
-            System.out.println("Using driver: "+System.getProperty("user.dir")+DRIVER_PATH);
-        } else {
-            System.out.println("Chrome driver not found in: "+System.getProperty("user.dir")+DRIVER_PATH);
-            System.out.println("Will attempt use the chrome driver that is in the system path.  If one doesn't exist, you will get an error on execution");
-        }
+//        File f = new File(System.getProperty("user.dir")+DRIVER_PATH);
+//        if(f.exists() && !f.isDirectory()) {
+//            // If you don't want to specify the path to the chrome driver, you will need to make sure it is part of the system path
+//            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+DRIVER_PATH);
+//            System.out.println("Using driver: "+System.getProperty("user.dir")+DRIVER_PATH);
+//        } else {
+//            System.out.println("Chrome driver not found in: "+System.getProperty("user.dir")+DRIVER_PATH);
+//            System.out.println("Will attempt use the chrome driver that is in the system path.  If one doesn't exist, you will get an error on execution");
+//        }
 
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
 
         // The following is what is needed to add the LeanFT reporting to your custom test framework.
